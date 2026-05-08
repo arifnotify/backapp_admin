d2594ad8767088831a7d00091587676d003b9165
arifnotify <arifnotify@gmail.com>
2026-05-08 23:25:13 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export default function Home() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const addProduct = async () => {
+    await fetch("https://backapp-pkmg.onrender.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, price: Number(price) }),
+    });
+
+    alert("Product Added");
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Add Product</h1>
+
+      <input
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        placeholder="Price"
+        onChange={(e) => setPrice(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={addProduct}>Add</button>
+    </div>
+  );
+}
